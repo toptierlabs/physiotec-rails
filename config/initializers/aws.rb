61a7fec618a567643b5b447184472cafbe1435ef
TopTier labs <TopTierlabs@TopTiers-Mac-mini-2.local>
2013-10-22 17:24:12 -0200

    Ignore .elasticbeanstalk from Git

---
+++ config/initializers/aws.rb
@@ -0,0 +1,4 @@
+# Be sure to restart your server when you modify this file.
+
+#Initializes the AWS gem with the required parameters
+AWS.config(access_key_id: 'AKIAISHIKVJWE3NYHUZQ', secret_access_key: 'fNHgOggsCDkvzqiW2qWB2EVIiR6n9RResTuZ5vxg', region: 'us-west-2')
