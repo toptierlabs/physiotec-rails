05435d6e666ecd1b416c2a7757f5454db65ab34f
TopTier labs <TopTierlabs@TopTiers-Mac-mini-2.local>
2013-12-18 16:19:20 -0200

    Validations on license model

--- app/models/license.rb
+++ app/models/license.rb
@@ -13,11 +13,20 @@ class License < ActiveRecord::Base
   validates :email, :first_name, :last_name, :maximum_clinics, :maximum_users, :phone, :api_license,
             :presence => true
 
-  validates :email, :uniqueness => {:scope => :api_license_id}
+  validates :email, :company_name, :uniqueness => {:scope => :api_license_id}
   validates :email, :email => true
+  validates :maximum_clinics, :maximum_users, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
 
+  validate :validate_clinics
+  def validate_clinics
+    errors.add(:clinics, "too much") if clinics.length > maximum_clinics
+  end
+
+  validate :validate_users
+  def validate_users
+    errors.add(:users, "too much") if users.length > maximum_users
+  end
   
-
   def license
   	self
   end
