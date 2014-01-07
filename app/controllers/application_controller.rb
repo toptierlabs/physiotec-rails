fc045a43f95124d05b1e1b322bc238d5bdbcf517
TopTier labs <TopTierlabs@TopTiers-Mac-mini-2.local>
2014-01-07 19:25:39 -0200

    app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,4 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery
 
-  #before_filter
-  #hsh.delete_if { |k, v| v.empty? }
-
-  def test(x)
-    hash_queue = Queue.new  
-    hash_queue << x
-    remove_keys = []
-    loop do
-      elem = hash_queue.pop
-      elem.keys.each do |v|
-        if elem[v].class == Hash
-          hash_queue.push(elem[v])
-        elsif elem[v].nil?
-          remove_keys << v
-        end
-        elem.except!(remove_keys)
-        puts remove_keys
-        remove_keys = []
-      end
-      break if hash_queue.empty?
-    end
-  end
-
 end
