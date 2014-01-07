b1796d09b25c9c37765e1a82b9a1898e011e1280
TopTier labs <TopTierlabs@TopTiers-Mac-mini-2.local>
2014-01-07 18:23:37 -0200

    app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery
 
-  before_filter
-  hsh.delete_if { |k, v| v.empty? }
+  #before_filter
+  #hsh.delete_if { |k, v| v.empty? }
 end
