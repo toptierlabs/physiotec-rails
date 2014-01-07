e0cfe07b39b195ee53a341283abe6920bfafe80c
TopTier labs <TopTierlabs@TopTiers-Mac-mini-2.local>
2014-01-07 19:09:20 -0200

    gemfile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,22 +5,23 @@ class ApplicationController < ActionController::Base
   #hsh.delete_if { |k, v| v.empty? }
 
   def test(x)
-  	hash_queue = Queue.new	
-  	hash_queue << x
-  	remove_keys = []
-  	loop do
-  		elem = hash_queue.pop
-  		elem.keys.each do |v|
-  			if elem[v].class == Hash
-  				hash_queue << elem[v]
-  			elsif elem[v].nil?
-  				remove_keys << v
-  			end
-  			elem.except!(remove_keys)
-  			remove_keys = []
-  		end
-  		break if hash_queue.empty?
-  	end
+    hash_queue = Queue.new  
+    hash_queue << x
+    remove_keys = []
+    loop do
+      elem = hash_queue.pop
+      elem.keys.each do |v|
+        if elem[v].class == Hash
+          hash_queue.push(elem[v])
+        elsif elem[v].nil?
+          remove_keys << v
+        end
+        elem.except!(remove_keys)
+        puts remove_keys
+        remove_keys = []
+      end
+      break if hash_queue.empty?
+    end
   end
 
 end
