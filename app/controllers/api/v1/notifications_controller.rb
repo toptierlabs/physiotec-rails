4cb58d012ed549aa3bd6db2f8433d08f58e1737d
Anthony Figueroa <afigueroa@toptierlabs.com>
2014-01-24 15:31:48 -0200

    Read notif

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -6,15 +6,25 @@ module Api
       # POST /notifications.json
       def create
         puts params
-        @notification = Notification.new
-        @notification.message_id = params["MessageId"]
-        @notification.topic_arn = params["TopicArn"]
-        @notification.subject = params["Subject"]
-        @notification.message = params["Message"]
-        @notification.extra = params.to_json
+        aws_request_body = JSON.parse(request.body.read())
+        puts '='*50
+        puts aws_request_body
+        puts '\n','-'*50
+        aws_message = JSON.parse(aws_request_body["Message"])
+        aws_et_job_id= aws_message["jobId"]
 
+        @notification = Notification.new
+        @notification.message_id = aws_request_body["MessageId"]
+        @notification.topic_arn = aws_request_body["TopicArn"]
+        @notification.subject = aws_request_body["Subject"]
+        @notification.message = aws_request_body["Message"]
+        @notification.extra = aws_request_body.to_json
+
+        
 
         if @notification.save
+          store_status(aws_et_job_id, aws_message["state"].upcase)
+          
           render json: @notification, status: :created
         else
           render json: @notification.errors.full_messages, status: :unprocessable_entity
@@ -24,6 +34,21 @@ module Api
       def index 
         render json: Notification.all
       end
+
+      private 
+
+        def store_status(job_id, state)
+          video = ExerciseVideo.where(job_id: aws_et_job_id).first
+        
+
+          if ((state == "COMPLETED") || (state ==  "WARNING"))   
+            video.status = ExerciseVideo::STATES[:completed]
+          elsif (aws_message["state"].upcase == "ERROR")
+            video.status = ExerciseVideo::STATES[:failed]
+          end
+          video.save
+
+        end 
     end
   end
 end
